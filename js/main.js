8f89024e4c610cdc83e2bc6105c125a098735895
Exxarah <sarahbeavan@gmail.com>
2021-05-29 12:44:57 +1200

    Fin

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+const scoreElement = document.querySelector('#scoreElement');
+const endScoreElement = document.querySelector('#endScoreElement');
+const startGameBtn = document.querySelector('#startGameBtn');
+const endGameElement = document.querySelector('#endGameElement');
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 class Circle {
-  constructor(x, y, radius, colour, velocity) {
+  constructor(x, y, radius, colour, velocity, friction = 1) {
     this.x = x;
     this.y = y;
 
     this.radius = radius;
     this.colour = colour;
     this.velocity = velocity;
+    this.friction = friction;
 
     this.alpha = 1;
   }
@@ -27,15 +32,30 @@ class Circle {
   }
 
   update(){
+    this.velocity.x *= this.friction;
+    this.velocity.y *= this.friction;
     this.x += this.velocity.x;
     this.y += this.velocity.y;
+
+    this.draw();
   }
 }
 
-const player = new Circle(canvas.width/2, canvas.height/2, 15, 'white', {x: 0, y: 0});
-const projectiles = [];
-const particles = [];
-const enemies = [];
+let player = new Circle(canvas.width/2, canvas.height/2, 15, '#cccccc', {x: 0, y: 0});
+let projectiles = [];
+let particles = [];
+let enemies = [];
+let score = 0;
+
+function init() {
+  player = new Circle(canvas.width/2, canvas.height/2, 15, '#cccccc', {x: 0, y: 0});
+  projectiles = [];
+  particles = [];
+  enemies = [];
+  score = 0;
+  scoreElement.innerHTML = score;
+  endScoreElement.innerHTML = score;
+}
 
 function spawnEnemies(){
   setInterval(() => {
@@ -66,21 +86,22 @@ function animate() {
 
   ctx.fillStyle = 'rgba(85, 85, 85, 0.1)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  player.draw();
 
+  player.update();
+
+  // Degrade Particles each frame
   particles.forEach((particle, index) => {
     particle.alpha -= 0.01;
     if (particle.alpha <= 0) {
       particles.splice(index, 1);
     } else {
       particle.update();
-      particle.draw();
     }
   })
 
+  // Tick projectiles, remove if out of bounds
   projectiles.forEach((projectile, index) => {
     projectile.update();
-    projectile.draw();
 
     // Remove if out of bounds
     if (
@@ -97,24 +118,29 @@ function animate() {
 
   enemies.forEach((enemy, index) => {
     enemy.update();
-    enemy.draw();
 
     // Check Enemy-Player Collision
     const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
     if (dist - enemy.radius - player.radius < 1) {
       cancelAnimationFrame(animationID);
+      endGameElement.style.display = 'flex';
+      endScoreElement.innerHTML = score;
     }
 
     // Check Enemy-Projectile Collision
     projectiles.forEach((projectile, pIndex) => {
       const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
       if (dist - enemy.radius - projectile.radius < 1) {
+        // Increase Score
+        score += 10;
+        scoreElement.innerHTML = score;
+
         // Spawn particle effects
         for (let i = 0; i < enemy.radius * 2; i++) {
           particles.push(new Circle(
             projectile.x, projectile.y,
-            Math.random() * 3, enemy.colour,
-            {x: Math.random() - 0.5, y: Math.random() - 0.5}
+            Math.random() * (5 - 2) + 2, enemy.colour,
+            {x: (Math.random() - 0.5) * 2, y: (Math.random() - 0.5) * 2}, 0.98
           ))
         }
 
@@ -140,7 +166,7 @@ window.addEventListener('click', (event) => {
   const x = canvas.width/2;
   const y = canvas.height/2;
   const radius = 5;
-  const colour = 'white';
+  const colour = '#cccccc';
   const angle = Math.atan2(event.clientY - canvas.height/2, event.clientX - canvas.width/2);
   const speed = 4;
   const velocity = {x: Math.cos(angle) * speed, y: Math.sin(angle) * speed};
@@ -148,5 +174,9 @@ window.addEventListener('click', (event) => {
   projectiles.push(new Circle(x, y, radius, colour, velocity))
 })
 
-animate();
-spawnEnemies();
+startGameBtn.addEventListener('click', () => {
+  endGameElement.style.display = 'none';
+  init();
+  animate();
+  spawnEnemies();
+})
